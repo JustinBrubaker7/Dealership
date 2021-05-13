da26cffb5165706d6d5932858ced57725f31062e
Justin Brubaker <justinbrubaker7@gmail.com>
2021-05-12 21:27:43 -0700

    user authentication

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const dealerRoutes = require("./dealerRoutes");
 const userRoutes = require("./userRoutes");
+const loginRoutes = require("./loginRoutes");
+const homeRoutes = require("./homeRoutes");
 
 router.use("/dealer", dealerRoutes);
 router.use("/user", userRoutes);
+router.use("/log", loginRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
