e427528d90576298385cc39eedd1d43bd552d722
Justin Brubaker <justinbrubaker7@gmail.com>
2021-05-14 16:02:44 -0700

    added routes for sold cars and authentication sytliung

--- controllers/dealerRoutes.js
+++ controllers/dealerRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require("../utils/auth");
 //dealer
 //user
 
-//returns hom dashboard page
+//returns home dashboard page
 router.get("/", withAuth, async (req, res) => {
   try {
     //if logged in
@@ -20,7 +20,7 @@ router.get("/", withAuth, async (req, res) => {
 });
 
 //returns all inventory for inventory tab
-router.get("/inventory", async (req, res) => {
+router.get("/inventory", withAuth, async (req, res) => {
   try {
     const carData = await Car.findAll();
 
@@ -28,7 +28,33 @@ router.get("/inventory", async (req, res) => {
 
     res.render("dealer-inventory", {
       inventory: true,
+      title: "Inventory",
       cars,
+      loggedIn: req.session.loggedIn,
+      layout: "main.handlebars",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//returns all sold inventory
+router.get("/sold", withAuth, async (req, res) => {
+  try {
+    const carData = await Car.findAll({
+      where: {
+        sold: true,
+      },
+    });
+
+    const cars = carData.map((carInfo) => carInfo.get({ plain: true }));
+
+    res.render("dealer-inventory", {
+      soldcars: true,
+      title: "Sold Cars",
+      cars,
+      loggedIn: req.session.loggedIn,
       layout: "main.handlebars",
     });
   } catch (err) {
@@ -38,23 +64,25 @@ router.get("/inventory", async (req, res) => {
 });
 
 //returns specific car by ID
-// router.get("/inventory/:id", async (req, res) => {
-//   try {
-//     const carData = await Car.findByPk(req.params.id);
+router.get("/:id", withAuth, async (req, res) => {
+  try {
+    const carData = await Car.findByPk(req.params.id);
+    console.log(carData);
+    const cars = carData.get({ plain: true });
+    console.log(cars);
+    res.render("dealer-profile", {
+      inventory: true,
+      cars,
+      loggedIn: req.session.loggedIn,
+      layout: "main.handlebars",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-//     const cars = carData.map((carInfo) => carInfo.get({ plain: true }));
-
-//     res.render("dealer-carprofile", {
-//       cars,
-//       layout: "main.handlebars",
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-router.put("/update/:id", async (req, res) => {
+router.put("/update/:id", withAuth, async (req, res) => {
   try {
     console.log("made it tot the back end");
     const updateCar = await Car.update(
@@ -68,6 +96,7 @@ router.put("/update/:id", async (req, res) => {
       }
     ).then((updateCar) => {
       res.redirect(200, "/dealer");
+
       res.status(200).json(updateCar);
       console.log("sending..");
     });
@@ -80,10 +109,11 @@ router.put("/update/:id", async (req, res) => {
 });
 
 //returns view to submit a new car
-router.get("/newcar", async (req, res) => {
+router.get("/newcar", withAuth, async (req, res) => {
   try {
     res.render("dealer-newcar", {
       newcar: true,
+      loggedIn: req.session.loggedIn,
       layout: "main.handlebars",
     });
   } catch (err) {
--- controllers/loginRoutes.js
+++ controllers/loginRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const dbUserData = await User.create({
       username: req.body.username,
